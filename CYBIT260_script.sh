78d9c4d963a01b907c384e07b2d629d6b847e12c
GentilS1415 <gentilskye@gmail.com>
2025-05-11 20:47:58 -0500

    Add files via upload

---
+++ CYBIT260_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello World!"
+echo "Hostname: $(hostname)"
+echo "Current date and time: $(date)"
+echo "System uptime: $(uptime)"
+echo "Linux Kernel Version: $(uname -r)"
+echo "Memory Information: $(free -h)"
+echo "Network Interface Information: $(ip address)"
